9d43830f0bc9c52efaa88d391f8b82def4c75f9c
Deniz Egemen Coşkun <denizegemencoskun@gmail.com>
2020-02-09 15:49:49 +0300

    Added like states.

--- App.js
+++ App.js
@@ -22,13 +22,11 @@ import {
 } from 'react-native';
 import Post from './components/Post';
 import Story from './components/Story';
-import { act } from 'react-test-renderer';
+
+
 
 
 const App = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [activeStory, setActiveStory] = useState(0);
-
   let marilynProfileImage = 'https://images.findagrave.com/photos/2012/149/725_133835273812.jpg';
   let mickjaggerProfileImage = 'https://www.inforum.com/incoming/3974800-2iwhmy-072419.F.FF.WAYBACK/alternates/BASE_LANDSCAPE/072419.F.FF.WAYBACK';
   let johnlennonProfileImage = 'https://pbs.twimg.com/profile_images/1181815686760472576/RQDqociu_400x400.jpg';
@@ -37,11 +35,32 @@ const App = () => {
   let mjacksonProfileImage = 'https://www.onthisday.com/images/people/michael-jackson-medium.jpg';
 
 
+  initialAllPosts = [
+    { id: 1, liked: false, username: 'marilyn', profileImage: { uri: marilynProfileImage }, content: { uri: 'https://i.insider.com/5ad8c03a1b60b453008b48b1?width=1136&format=jpeg' }, postDate: '13 hours ago', comments: [{ id: 23423424, username: 'marilyn', content: 'New York City' }, { id: 23565324, username: 'johnlennon', content: 'Nice photo', subcomments: [{ id: 234234, username: 'mickjagger', content: 'I agree with you' }, { id: 23576564543, username: 'marilyn', content: 'Thank you both' }] }] },
+    { id: 2, liked: false, username: 'mickjagger', profileImage: { uri: mickjaggerProfileImage }, content: { uri: 'https://cdn.getyourguide.com/img/location_img-173-2017400448-148.jpg' }, postDate: '3 days ago', comments: [{ id: 23423424, username: 'mickjagger', content: 'Dubai' }, { id: 23565324, username: 'johnlennon', content: 'Which country is this city in?', subcomments: [{ id: 234234, username: 'mickjagger', content: 'United Arab Emirates' }, { id: 23576564543, username: 'johnlennon', content: 'Yes, right' }] }, { id: 7489238, username: 'marilyn', content: 'How did you take this shot?', subcomments: [{ id: 2342948, username: 'mickjagger', content: 'From a helicopter' }] }] },
+    { id: 3, liked: true, username: 'johnlennon', profileImage: { uri: johnlennonProfileImage }, content: { uri: 'https://www.jetsetter.com/uploads/sites/7/2019/04/GettyImages-924894324-1380x690.jpg' }, postDate: '5 days ago', comments: [{ id: 748734, username: 'johnlennon', content: 'Paris' }] },
+    { id: 4, liked: false, username: 'axlrose', profileImage: { uri: axlroseProfileImage }, content: { uri: 'http://landmarkbranding.com/wp-content/uploads/2016/02/The-GE-Building-at-30-Rockefeller-Plaza-768x1365.jpg' }, postDate: '6 days ago' },
+    { id: 5, liked: false, username: 'johnlennon', profileImage: { uri: johnlennonProfileImage }, content: { uri: 'https://www.history.com/.image/t_share/MTU3ODc5MDg2NDMxODcyNzM1/egyptian-pyramids-hero.jpg' }, postDate: '7 days ago', comments: [{ id: 83475, username: 'johnlennon', content: 'Pyramids' }] }
+  ];
+
+  const [allPosts, setAllPosts] = useState(initialAllPosts);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [activeStory, setActiveStory] = useState(0);
+
+  const handleLike = (postId) => {
+    //Bu fonksiyon network request'i taklit ediyor.
+    index = [allPosts.findIndex(post => post.id == postId)];
+    allPosts[index].liked = !allPosts[index].liked;
+    setAllPosts(allPosts);
+  }
+
+
+
   let iconSize = 28;
 
   const renderPost = itemData => {
     return (
-      <Post data={itemData.item} />
+      <Post handleLike={handleLike} data={itemData.item} />
     )
   }
 
@@ -73,13 +92,7 @@ const App = () => {
       />)
   }
 
-  postData = [
-    { id: 1, username: 'marilyn', profileImage: { uri: marilynProfileImage }, content: { uri: 'https://i.insider.com/5ad8c03a1b60b453008b48b1?width=1136&format=jpeg' }, postDate: '13 hours ago', comments: [{ id: 23423424, username: 'marilyn', content: 'New York City' }, { id: 23565324, username: 'johnlennon', content: 'Nice photo', subcomments: [{ id: 234234, username: 'mickjagger', content: 'I agree with you' }, { id: 23576564543, username: 'marilyn', content: 'Thank you both' }] }] },
-    { id: 2, username: 'mickjagger', profileImage: { uri: mickjaggerProfileImage }, content: { uri: 'https://cdn.getyourguide.com/img/location_img-173-2017400448-148.jpg' }, postDate: '3 days ago', comments: [{ id: 23423424, username: 'mickjagger', content: 'Dubai' }, { id: 23565324, username: 'johnlennon', content: 'Which country is this city in?', subcomments: [{ id: 234234, username: 'mickjagger', content: 'United Arab Emirates' }, { id: 23576564543, username: 'johnlennon', content: 'Yes, right' }] }, { id: 7489238, username: 'marilyn', content: 'How did you take this shot?', subcomments: [{ id: 2342948, username: 'mickjagger', content: 'From a helicopter' }] }] },
-    { id: 3, username: 'johnlennon', profileImage: { uri: johnlennonProfileImage }, content: { uri: 'https://www.jetsetter.com/uploads/sites/7/2019/04/GettyImages-924894324-1380x690.jpg' }, postDate: '5 days ago', comments: [{ id: 748734, username: 'johnlennon', content: 'Paris' }] },
-    { id: 4, username: 'axlrose', profileImage: { uri: axlroseProfileImage }, content: { uri: 'http://landmarkbranding.com/wp-content/uploads/2016/02/The-GE-Building-at-30-Rockefeller-Plaza-768x1365.jpg' }, postDate: '6 days ago' },
-    { id: 5, username: 'johnlennon', profileImage: { uri: johnlennonProfileImage }, content: { uri: 'https://www.history.com/.image/t_share/MTU3ODc5MDg2NDMxODcyNzM1/egyptian-pyramids-hero.jpg' }, postDate: '7 days ago', comments: [{ id: 83475, username: 'johnlennon', content: 'Pyramids' }] }
-  ];
+
 
   storiesData = [
     { id: 0, username: 'Your Story', profileImage: { uri: marilynProfileImage }, content: { uri: 'https://images.unsplash.com/photo-1496449903678-68ddcb189a24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60' } },
@@ -132,7 +145,7 @@ const App = () => {
         <FlatList
           style={{ padding: 0, margin: 0 }}
           nestedScrollEnabled={true}
-          data={postData}
+          data={allPosts}
           keyExtractor={(item, index) => { return '' + item.id; }}
           renderItem={renderPost}
           ListHeaderComponent={renderListHeader_StoriesList}
@@ -144,7 +157,7 @@ const App = () => {
         <Image source={require('./assets/icons/search.png')} style={{ width: iconSize, height: iconSize }} />
         <Image source={require('./assets/icons/add.png')} style={{ width: iconSize, height: iconSize }} />
         <Image source={require('./assets/icons/like-empty.png')} style={{ width: iconSize, height: iconSize }} />
-        <Image source={{uri: marilynProfileImage}} style={{ height: iconSize, width: iconSize, borderRadius: 100 }}></Image>
+        <Image source={{ uri: marilynProfileImage }} style={{ height: iconSize, width: iconSize, borderRadius: 100 }}></Image>
 
       </View>
     </SafeAreaView >
--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   SafeAreaView,
@@ -9,19 +9,23 @@ import {
   Image,
   StatusBar,
   FlatList,
+  TouchableOpacity,
   Dimensions
 } from 'react-native';
 import FullWidthImage from 'react-native-fullwidth-image';
 import AutoHeightImage from 'react-native-auto-height-image';
 
 const Post = props => {
+
+  const [liked, setLiked] = useState(props.data.liked);
+
   const renderComment = itemData => {
     return (
       <View style={{ width: '100%' }}>
         <Text><Text style={{ fontWeight: 'bold' }}>{itemData.item.username}</Text> {itemData.item.content}</Text>
         <FlatList
           data={itemData.item.subcomments}
-          keyExtractor={(item, index) => {return ''+item.id;}}
+          keyExtractor={(item, index) => { return '' + item.id; }}
           renderItem={itemData2 => {
             return (<Text><Text style={{ fontWeight: 'bold' }}>   |   {itemData2.item.username}</Text> {itemData2.item.content}</Text>);
           }}
@@ -30,18 +34,27 @@ const Post = props => {
     )
   }
 
+  function informParentOfLike() {
+    props.handleLike(props.data.id);
+  }
+
   let iconSize = 28;
   return (
     <View style={styles.container}>
       <View style={styles.ownerBar}>
         <Image source={props.data.profileImage} style={{ height: iconSize + 12, width: iconSize + 12, borderRadius: 100 }}></Image>
         <Text style={{ paddingLeft: 12, flex: 1, fontWeight: 'bold' }}>{props.data.username}</Text>
-        <Image source={require('../assets/icons/options.png')} style={{ width: iconSize, height: iconSize}} />
+        <Image source={require('../assets/icons/options.png')} style={{ width: iconSize, height: iconSize }} />
 
       </View>
       <FullWidthImage source={props.data.content} />
       <View style={styles.actionsBar}>
-        <Image source={require('../assets/icons/like-empty.png')} style={{ width: iconSize, height: iconSize, marginRight: 8 }} />
+        <TouchableOpacity onPress={() => {setLiked(!liked); informParentOfLike();}}>
+          <Image
+            source={liked ? require('../assets/icons/like-filled.png') : require('../assets/icons/like-empty.png')}
+            style={{ width: iconSize, height: iconSize, marginRight: 8 }}
+          />
+        </TouchableOpacity>
         <Image source={require('../assets/icons/comment.png')} style={{ width: iconSize, height: iconSize, marginRight: 8 }} />
         <Image source={require('../assets/icons/send.png')} style={{ width: iconSize, height: iconSize }} />
         <Image source={require('../assets/icons/bookmark.png')} style={{ width: iconSize, height: iconSize, position: 'absolute', right: 8 }} />
@@ -51,7 +64,7 @@ const Post = props => {
         <FlatList
           style={{ width: '100%' }}
           data={props.data.comments}
-          keyExtractor={(item, index) => {return ''+item.id;}}
+          keyExtractor={(item, index) => { return '' + item.id; }}
           renderItem={renderComment}
         >
         </FlatList>
